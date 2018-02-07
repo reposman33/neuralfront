b1197a948459615603bff9466172fdddc0a0d82c
reposman33 <info@marcbakker.com>
2018-02-07 20:05:45 +0100

    Added /shared/ folder and images

---
+++ src/app/Shared/button-i18n.model.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ButtonI18nModel {
+  public texts = {
+    'cameraButton': {buttonText: '', titleText: ''},
+    'photoButton': {buttonText: '', titleText: ''}
+  };
+
+  constructor() {
+  }
+
+}
---
+++ src/app/Shared/i18n.model.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class I18nModel {
+  public 'header-button-camera' = {'value': "",'title': ""};
+  public 'header-button-photo' = {'value': "", 'title': ""};
+
+  constructor() {}
+
+}
